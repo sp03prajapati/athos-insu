44420390b89670ce31ccdb1aa4619f20fa941a7b
jungle mangal <shailesh.mitra78@gmail.com>
2024-11-26 14:02:09 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import MainCenter from './components/MainSection.vue';
+import Footer from './components/Footer.vue';
+import ImageSlider from './components/ImageSlider.vue'
+</script>
+
+<template>
+  <header>
+    <Navbar/>
+  </header>
+  <main>
+    <MainCenter/>
+  </main>
+  <Footer/>
+</template>
