0c0b2badaf6ef29cefc7a46cc96b5cf998cee53a
jungle mangal <shailesh.mitra78@gmail.com>
2024-11-28 11:07:29 +0530

    we change API url

--- src/components/NewsLetter.vue
+++ src/components/NewsLetter.vue
@@ -17,7 +17,7 @@ export default {
     },
     methods: {
         GetNews() {
-            axios.get('http://onestopcar.somee.com/weatherforecast/GetFeedback').then(res => {
+            axios.get('https://www.onestopcar.somee.com/WeatherForecast/getFeedback').then(res => {
                 console.log(res.data);
                 this.result=res.data;
                 console.log(this.result);
@@ -41,7 +41,7 @@ export default {
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        Accordion Item #1
+        Offer #1
       </button>
     </h2>
     <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
@@ -51,7 +51,7 @@ export default {
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        Accordion Item #2
+        Offer #2
       </button>
     </h2>
     <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
